dee2a1df2f9cdd25f0285c4f66e06bc5913c6c0a
wverlaek <wverlaek@hotmail.nl>
2018-03-01 17:46:56 +0000

    Input files

--- QualificationRound2018/src/InputFiles.java
+++ QualificationRound2018/src/InputFiles.java
@@ -1,3 +1,7 @@
 public class InputFiles {
-    public static final String FILE_0 = "file_0";
+    public static final String A_EXAMPLE = "a_example";
+    public static final String B_SHOULD_BE_EASY = "b_should_be_easy";
+    public static final String C_NO_HURRY = "c_no_hurry";
+    public static final String D_METROPOLIS = "d_metropolis";
+    public static final String E_HIGH_BONUS = "e_high_bonus";
 }
