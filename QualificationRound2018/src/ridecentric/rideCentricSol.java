803f91665b2a7c89d3b648d0a1d5a147140a3b4a
NouddeKroon <achtvijf65>
2018-03-01 19:29:23 +0000

    Added ride centric ride solver.

---
+++ QualificationRound2018/src/ridecentric/rideCentricSol.java
@@ -0,0 +1,11 @@
+package ridecentric;
+
+import base.RidesSolver;
+import output.SelfDrivingSolution;
+
+public class rideCentricSol extends RidesSolver {
+    @Override
+    public SelfDrivingSolution solve() {
+        return null;
+    }
+}
