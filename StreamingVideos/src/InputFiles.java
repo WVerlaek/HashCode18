c5b567e8e70be91fee94874b6ca5c0980086bb29
wverlaek <wverlaek@hotmail.nl>
2018-03-01 17:22:54 +0000

    Move 2017 input files to own directory

--- StreamingVideos/src/InputFiles.java
+++ StreamingVideos/src/InputFiles.java
@@ -1,6 +1,8 @@
 public class InputFiles {
-    public static final String KITTENS = "kittens";
-    public static final String ME_AT_THE_ZOO = "me_at_the_zoo";
-    public static final String TRENDING_TODAY = "trending_today";
-    public static final String VIDEOS_WORTH_SPREADING = "videos_worth_spreading";
+    private static final String DIR = "hc2017/";
+
+    public static final String KITTENS = DIR + "kittens";
+    public static final String ME_AT_THE_ZOO = DIR + "me_at_the_zoo";
+    public static final String TRENDING_TODAY = DIR + "trending_today";
+    public static final String VIDEOS_WORTH_SPREADING = DIR + "videos_worth_spreading";
 }
